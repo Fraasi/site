9676c83653c20174279c72ef0f47726cc43b8bb1
fraasi <doofassi@gmail.com>
2021-07-20 20:51:52 +0300

    optional coverimage

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -17,7 +17,8 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
       <div className="hidden md:block md:mb-12">
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
+        {coverImage ? (<CoverImage title={title} src={coverImage} />) : 'No coverImage'}
+
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
