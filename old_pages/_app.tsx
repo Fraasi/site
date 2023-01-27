45cdee90156a11cac162ad3b592bc02af6f33990
fraasi <fraasi.gh@gmail.com>
2023-01-27 22:21:16 +0200

    remove old files and cleanup bootsrap css

--- old_pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-import '../styles/prism.css'
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
